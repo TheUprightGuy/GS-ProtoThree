f515da04fadc44960fc8e4e307a19a36351375b2
BigGriz <waydbarton@hotmail.co.nz>
2020-08-10 18:17:03 +1200

    Better Camera

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -4,24 +4,48 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    public GameObject target;
-    public float speed = 5;
+    [Header("Required Fields")]
+    public GameObject whaleBody;
+    public Transform layerCamPos;
 
+    [Header("Debug Fields")]
+    public bool zoomIn = false;
+    public float speed = 5;
     Vector3 offset;
 
     void Start()
     {
-        offset = target.transform.position - transform.position;
+        offset = whaleBody.transform.position - transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            zoomIn = true;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < -0f)
+        {
+            zoomIn = false;
+        }
     }
 
     void LateUpdate()
-    {
-        // Look
-        var newRotation = Quaternion.LookRotation(target.transform.position - transform.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, speed * Time.deltaTime);
+    {     
+        if (zoomIn)
+        {
+            transform.position = Vector3.Lerp(transform.position, layerCamPos.position, speed * Time.deltaTime / 2);
+            transform.rotation = Quaternion.Slerp(transform.rotation, layerCamPos.rotation, speed * Time.deltaTime);
+        }
+        else
+        {
+            // Look
+            var newRotation = Quaternion.LookRotation(whaleBody.transform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, speed * Time.deltaTime);
 
-        // Move
-        Vector3 newPosition = target.transform.position - target.transform.forward * offset.z - target.transform.up * offset.y;
-        transform.position = Vector3.Slerp(transform.position, newPosition, Time.deltaTime * speed);
+            // Move
+            Vector3 newPosition = whaleBody.transform.position - whaleBody.transform.forward * offset.z - whaleBody.transform.up * offset.y;
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * speed / 2);
+        }
     }
 }
