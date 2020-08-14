f84e864cb72f0e0cfc2a016b5943110f9d755cd5
BigGriz <waydbarton@hotmail.co.nz>
2020-08-14 21:36:59 +1200

    Skybox Blending

---
+++ Assets/Materials/Skyboxes/SkyboxBlender.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxBlender : MonoBehaviour
+{
+    public Material material;
+    public float blend;
+    public float count;
+    public float blendSpeed = 1.0f;
+
+    void Start()
+    {
+        material.SetFloat("_Blend", 0f);
+    }
+
+    void Update()
+    {
+        count += Time.deltaTime * blendSpeed;
+
+        blend = Mathf.PingPong(count, 1.0f);
+        material.SetFloat("_Blend", blend);
+    }
+}
+
