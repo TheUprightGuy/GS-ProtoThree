e2bbd80302019f9d44ced18461bd76d7c59234d1
Jack <jack.belton@hotmail.com>
2020-08-14 20:32:18 +1200

    Catchup

---
+++ Assets/Scripts/ResourceCollector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceCollector : MonoBehaviour
+{
+    WhaleInfo whaleInfo;
+    ResourceDisplayScript rds;
+
+    public List<GameObject> supplyObjs;
+    public List<GameObject> provisionObjs;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        whaleInfo = CallbackHandler.instance.whaleInfo;
+        rds = ResourceDisplayScript.instance;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
