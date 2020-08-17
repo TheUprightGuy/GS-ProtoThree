0bd547ca17e199fcaaf3dd8cbc9d190924d85b18
BigGriz <waydbarton@hotmail.co.nz>
2020-08-17 12:10:56 +1200

    Anim & Controller In - Not Scripted Yet

---
+++ Assets/CharacterControllerScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+    public Animator animator;
+    public bool moving, standing, sitting, steering;
+    public bool stand, sit, sitTransition, feed;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        UpdateAnimState();
+    }
+
+    public void UpdateAnimState()
+    {
+        animator.SetBool("Moving", moving);
+        animator.SetBool("Standing", standing);
+        animator.SetBool("Sitting", sitting);
+        animator.SetBool("Steering", steering);
+
+        if (stand)
+        {
+            animator.SetTrigger("StandUp");
+            stand = false;
+            standing = true;
+            sitting = false;
+        }
+        if (sit)
+        {
+            animator.SetTrigger("SitDown");
+            sit = false;
+            sitting = true;
+            standing = false;
+        }
+        if (sitTransition)
+        {
+            animator.SetTrigger("SitTransition");
+            sitTransition = false;
+        }
+        if (feed)
+        {
+            animator.SetTrigger("FeedWhale");
+            feed = false;
+        }
+    }
+}
