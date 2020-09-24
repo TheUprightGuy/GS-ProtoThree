5c984166c9ca28ca6dea2490087388498b251eea
BigGriz <waydbarton@hotmail.co.nz>
2020-09-24 15:28:50 +1200

    Using Cinemachine

---
+++ Assets/TestCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class TestCam : MonoBehaviour
+{
+    CinemachineFreeLook cam;
+    private void Awake()
+    {
+        cam = GetComponent<CinemachineFreeLook>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.m_XAxis.m_MaxSpeed = 0;
+        cam.m_YAxis.m_MaxSpeed = 0;
+
+        if (Input.GetMouseButton(0))
+        {
+            cam.m_XAxis.m_MaxSpeed = 300;
+            cam.m_YAxis.m_MaxSpeed = 2;
+        }
+    }
+}
