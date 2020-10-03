0ac086eae2a27265c7155c45cdedd40d26480d14
Jack <Jack>
2020-10-03 16:26:12 +1300

    ObjDistribution tools done

--- Assets/Scripts/ObjDistribuition.cs
+++ Assets/Scripts/ObjDistribuition.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-
+using Unity.Jobs;
+using Unity.Collections;
 
 public class ObjData
 {
@@ -34,7 +35,7 @@ public class ObjData
     [ExecuteAlways]
     public class ObjDistribuition : MonoBehaviour
     {
-
+    
     public float minScale = 1.0f;
     public float maxScale = 1.0f;
 
@@ -44,34 +45,57 @@ public class ObjData
 
     public GameObject prefabTemplate;
 
-    private List<List<ObjData>> batches = new List<List<ObjData>>();
+    [SerializeField]
+    private List<List<ObjData>> batches;
 
+    [HideInInspector]
+    public Bounds BoundingBox;
     private void Start()
     {
-        PlaceObjs(transform.position);
+        PlaceObjs();
     }
+
     private void Update()
     {
         Redraw();
     }
 
-    private void Redraw()
+    public void Redraw()
     {
-        foreach (var batch in batches)
+        if (batches != null)
         {
-            Graphics.DrawMeshInstanced(prefabTemplate.GetComponentInChildren<MeshFilter>().sharedMesh, 0, prefabTemplate.GetComponentInChildren<MeshRenderer>().sharedMaterial, batch.Select((a) => a.matrix).ToList());
+            foreach (var batch in batches)
+            {
+                Graphics.DrawMeshInstanced(prefabTemplate.GetComponentInChildren<MeshFilter>().sharedMesh, 0, prefabTemplate.GetComponentInChildren<MeshRenderer>().sharedMaterial, batch.Select((a) => a.matrix).ToList());
+            }
         }
+        
     }
 
-
-    public void PlaceObjs(Vector3 pos)
+    public void PlaceObjs()
     {
+        if (batches == null)
+        {
+            Debug.Log("Making Array");
+            batches = new List<List<ObjData>>();
+        }
+
+        if (batches.Count > 0)
+        {
+            Debug.Log("Clearing Array");
+            batches.Clear();
+        }
+
+        Debug.Log("Randomising Positions...");
         List<Vector3> PointList = ReRoll();
+        Debug.Log("Randomising Positions Done");
 
-        int batchIndexNum = 0;
+        PointList = RaycastPositions(PointList);
 
+        
         List<ObjData> currBatch = new List<ObjData>();
 
+        int batchIndexNum = 0;
         for (int i = 0; i < PointList.Count; i++)
         {
 
@@ -80,7 +104,8 @@ public class ObjData
             foreach (Transform item in prefabTemplate.transform)
             {
 
-                currBatch.Add(new ObjData(pos + PointList[i], item.localScale * randScale, item.rotation));
+                //Apply prefabs positions AFTER raycast for offset
+                currBatch.Add(new ObjData( PointList[i] + prefabTemplate.transform.position, item.localScale * randScale, item.rotation)); ;
                 batchIndexNum++;
             }
 
@@ -97,44 +122,86 @@ public class ObjData
             batches.Add(currBatch);
         }
 
+
+        Redraw();
     }
 
     public List<Vector3> ReRoll()
     {
         List<Vector3> PointList = new List<Vector3>();
 
-        float stepSize = discDiameter / density;
-        for (float i = -(density / 2); i < density / 2; i++)
+        float stepSizeX = transform.localScale.x / density;
+        float stepSizeZ = transform.localScale.z / density;
+        for (float i = -(density / 2); i < density / 2; i++) //have inital position been in center, rather than up and left
         {
             for (float j = -(density / 2); j < density / 2; j++)
             {
                 //Get random point in cell.
-                float randX = Random.Range(i * stepSize, (i + 1) * stepSize);
-                float randZ = Random.Range(j * stepSize, (j + 1) * stepSize);
+                float randX = Random.Range(i * stepSizeX, (i + 1) * stepSizeX);
+                float randZ = Random.Range(j * stepSizeZ, (j + 1) * stepSizeZ);
 
-                Vector3 point = new Vector3(randX, 0.0f, randZ);
-                PointList.Add(point);
+                Vector3 point = new Vector3(randX, transform.position.y, randZ);
+                
+                //Apply local position
+                PointList.Add(transform.position  + point);
 
-                ////Restrict to within circle
-                //if (Vector3.Distance(point, Vector3.zero) < discDiameter)
-                //{
-                //    PointList.Add(point);
-                //}
             }
         }
 
         return PointList;
     }
+    public LayerMask mask = -1;
+    public List<Vector3> RaycastPositions(List<Vector3> posList)
+    {
 
+        //This chunk doesn't work
+        //It is a COPY PASTE of the docs
+        //Nice job unity. Single threaded it is
+
+
+        //// Perform a single raycast using RaycastCommand and wait for it to complete
+        //// Setup the command and result buffers
+        //var results = new NativeArray<RaycastHit>(posList.Count, Allocator.TempJob);
+        //var commands = new NativeArray<RaycastCommand>(posList.Count, Allocator.TempJob);
+
+
+
+        //Vector3 direction = Vector3.down;
+
+        //for (int i = 0; i < posList.Count; i++)
+        //{            
+        //    commands[i] = new RaycastCommand(transform.position, Vector3.down, Mathf.Infinity, mask);
+        //}
+
+        //Debug.Log("Scheduling Raycasts...");
+        //// Schedule the batch of raycasts
+        //JobHandle handle = RaycastCommand.ScheduleBatch(commands, results, commands.Length, default(JobHandle));
+
+        //// Wait for the batch processing job to complete
+        //handle.Complete();
+        //Debug.Log("RayCasts Complete");
+
+        List<Vector3> returnList = new List<Vector3>();
+
+        //// Copy the result. If batchedHit.collider is null there was no hit
+        //RaycastHit batchedHit = results[0];
+
+        Debug.Log("Scheduling Raycasts...");
+        for (int i = 0; i < posList.Count; i++)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(posList[i], Vector3.down, out hit, Mathf.Infinity))
+            {
+                returnList.Add(hit.point);
+            }
+        }
+        Debug.Log("RayCasts Complete");
+
+        return returnList;
+    }
 
     private void OnDrawGizmos()
     {
-        foreach (var batch in batches)
-        {
-            foreach (var item in batch)
-            {
-                Gizmos.DrawSphere(item.pos, 1.0f);
-            }
-        }
+       
     }
 }
