ec926b8e56a4d598fc820fb7fbb978e10ca6694a
BigGriz <waydbarton@hotmail.co.nz>
2020-10-04 13:59:02 +1300

    Basic Switch Puzzle

---
+++ Assets/PuzzleSwitch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleSwitch : MonoBehaviour
+{
+
+    public List<PuzzleSwitch> adjacentSwitches;
+
+    [HideInInspector] public bool active;
+    [HideInInspector] public Material on;
+    [HideInInspector] public Material off;
+
+    MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void Use()
+    {
+        Switch();
+
+        foreach (PuzzleSwitch n in adjacentSwitches)
+        {
+            n.Switch();
+        }
+    }
+
+    public void Switch()
+    {
+        if (!SwitchPuzzleMaster.instance.complete)
+        {
+            active = !active;
+            meshRenderer.material = active ? on : off;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Use();
+        if (SwitchPuzzleMaster.instance.CheckComplete())
+        {
+            Debug.Log("Complete!");
+        }
+    }
+}
