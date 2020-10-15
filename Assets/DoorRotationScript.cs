4aef3257aacf7e175bf88ff6994f149aa38078d3
BigGriz <waydbarton@hotmail.co.nz>
2020-10-15 15:28:31 +1300

    Door Anim

---
+++ Assets/DoorRotationScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorRotationScript : MonoBehaviour
+{
+    public GameObject door;
+
+    public bool rotatingLocks;
+    public bool slidingDoors;
+    public float rotato;
+    public GameObject buttonLock;
+    public GameObject middleLock;
+    public GameObject outsideLock;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotatingLocks)
+        {
+            if (buttonLock.transform.eulerAngles.z >= -180 && buttonLock.transform.eulerAngles.z <= 180)
+            {
+                outsideLock.transform.eulerAngles += new Vector3(0, 0, 50) * Time.deltaTime * 3;
+
+                middleLock.transform.eulerAngles += new Vector3(0, 0, 50) * Time.deltaTime * 2;
+
+                buttonLock.transform.eulerAngles += new Vector3(0, 0, 50) * Time.deltaTime;
+            }
+            else
+            {
+                rotatingLocks = false;
+                slidingDoors = true;
+            }
+        }
+        if (slidingDoors)
+        {
+            if (door.transform.localPosition.x > -1.0f)
+            {
+                buttonLock.transform.localPosition += new Vector3(0, 0, 0.1f) * Time.deltaTime;
+                door.transform.localPosition += new Vector3(-1, 0, 0) * Time.deltaTime;
+            }
+        }
+
+    }
+}
---
+++ Assets/SlidingDoorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingDoorScript : MonoBehaviour
+{
+    public bool rotatingLocks;
+    public bool slidingDoors;
+    public float rotato;
+    public DoorRotationScript door1;
+    public DoorRotationScript door2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            RotateLocks();
+        }
+    }
+
+    public void RotateLocks()
+    {
+        door1.rotatingLocks = true;
+        door2.rotatingLocks = true;
+    }
+}
