6e53f3286935eff45e9491a62ef78cab9949d146
Jack <Jack>
2020-10-29 22:37:12 +1300

    Minimap tracker finished

--- Assets/Scripts/UI/MapHandler.cs
+++ Assets/Scripts/UI/MapHandler.cs
@@ -1,28 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.UI;
 
+
+[System.Serializable]
+public class ObjectiveTrackers
+{
+    public string ObjectiveName;
+    public Transform ObjectiveTransformInWorld;
+    public GameObject ObjectivePointerOnRing;
+    public GameObject ObjectiveMarkerOnMap;
+}
 public class MapHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("World Corners")]
     public Vector2 WorldTopLeft = Vector3.zero;
     public Vector2 WorldBottomRight = Vector2.one;
 
-    public Vector2 percentPos = Vector2.zero;
+    private Vector2 percentPos = Vector2.zero;
 
+    [Header("MiniMap Tracker")]
     public Transform trackingTransform;
 
-    Vector3 centerPos = Vector3.zero;
-    Bounds MapMeshBounds;
+    [Header("Pointer Fade Settings")]
+    public float MiniMapWorldSize = 100.0f;
+    public float fadeStartDistance = 80.0f;
+
+    private int ActiveObjectiveIndex = 0;
+    [Space(10)]
+    public ObjectiveTrackers[] Objectives;
+
     void Start()
     {
-        centerPos = transform.GetComponent<RectTransform>().localPosition;
-        //MapMeshBounds = GetComponent<MeshFilter>().sharedMesh.bounds;
+        for (int i = 0; i < Objectives.Length; i++)
+        {
+            if (i != ActiveObjectiveIndex)
+            {
+                Objectives[i].ObjectiveMarkerOnMap.active = false;
+                Objectives[i].ObjectivePointerOnRing.active = false;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float lengthX = WorldBottomRight.x - WorldTopLeft.x;
         float lengthY = WorldTopLeft.y - WorldBottomRight.y;
 
@@ -39,6 +64,37 @@ public class MapHandler : MonoBehaviour
         Vector3 trackingPos = new Vector3(GetComponent<RectTransform>().rect.width * percentX, GetComponent<RectTransform>().rect.height * percentY); ;
         Vector3 pos = new Vector3(startPos.x - trackingPos.x, (startPos.y - trackingPos.y), 0.0f);
         GetComponent<RectTransform>().localPosition = pos;
+
+
+        float dist = Vector3.Distance(Objectives[ActiveObjectiveIndex].ObjectiveTransformInWorld.position, trackingTransform.position);
+        Objectives[ActiveObjectiveIndex].ObjectivePointerOnRing.active = (dist) > (fadeStartDistance);
+
+        if (Objectives[ActiveObjectiveIndex].ObjectivePointerOnRing.GetComponent<Image>())
+        {
+            Color newCol = Objectives[ActiveObjectiveIndex].ObjectivePointerOnRing.GetComponent<Image>().color;
+            newCol.a = (dist - fadeStartDistance) / (MiniMapWorldSize - fadeStartDistance);
+            Objectives[ActiveObjectiveIndex].ObjectivePointerOnRing.GetComponent<Image>().color = newCol;
+        }
+
+    }
+
+    /// <summary>
+    /// Sets the current active objective.
+    /// </summary>
+    /// <param name="ObjectiveIndex">Index for objective to set active in the objectives array.</param>
+    /// <returns>Returns false if an invalid Index is passed, and true if objective setting is succesful.</returns>
+    public bool SetActiveObjective(int ObjectiveIndex)
+    {
+        if (ObjectiveIndex > 0 && ObjectiveIndex < Objectives.Length)
+        {
+            //Deactivate the previous marks
+            Objectives[ActiveObjectiveIndex].ObjectiveMarkerOnMap.active = false;
+            Objectives[ActiveObjectiveIndex].ObjectivePointerOnRing.active = false;
+
+            ActiveObjectiveIndex = ObjectiveIndex;
+            return true;
+        }
+        return false;
     }
 
     private void OnDrawGizmos()
@@ -46,5 +102,7 @@ public class MapHandler : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(new Vector3(WorldTopLeft.x, 0, WorldTopLeft.y), 10.0f);
         Gizmos.DrawSphere(new Vector3(WorldBottomRight.x, 0, WorldBottomRight.y), 10.0f);
+
+        Gizmos.DrawWireSphere(trackingTransform.position, MiniMapWorldSize);
     }
 }
